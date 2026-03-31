e04009cab0b1c3adda6a958cb86532701d8dcfb0
汉堡包 <syahirfan545@gmail.com>
2026-03-31 22:36:41 +0700

    Add BMI classification for normal weight range in classifyBMI function

--- bmiConditionalStatement.cpp
+++ bmiConditionalStatement.cpp
@@ -8,3 +8,5 @@ float calculateBMI(float weight, float height) { // Fungsi untuk menghitung BMI
 string classifyBMI(float bmi) { // Fungsi untuk mengklasifikasikan BMI berdasarkan nilai yang dihitung
     if (bmi < 18.5) { // Jika BMI kurang dari 18.5, maka status berat badan adalah "Kurang"
         return "Kurang";
+    } else if (bmi >= 18.5 && bmi <= 24.9) { // Jika BMI antara 18.5 dan 24.9, maka status berat badan adalah "Normal"
+        return "Normal";
