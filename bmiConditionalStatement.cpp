5f630401ad824102bf6c11e497ea283d9b68fdbe
汉堡包 <syahirfan545@gmail.com>
2026-03-31 22:35:41 +0700

    Add BMI calculation function to bmiConditionalStatement.cpp

--- bmiConditionalStatement.cpp
+++ bmiConditionalStatement.cpp
@@ -1,3 +1,7 @@
 #include <iostream> // Library untuk input dan output
 using namespace std; // Menggunakan namespace std untuk menghindari penulisan std:: sebelum setiap elemen dari library
 
+float calculateBMI(float weight, float height) { // Fungsi untuk menghitung BMI
+    return weight / (height * height); // Rumus BMI: berat badan (kg) dibagi dengan tinggi badan (m) kuadrat
+}
+
