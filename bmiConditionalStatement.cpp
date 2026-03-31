c089f106485e0589dac2c97a617aa4ae1f39824c
汉堡包 <syahirfan545@gmail.com>
2026-03-31 22:36:29 +0700

    Implement BMI classification function in bmiConditionalStatement.cpp

--- bmiConditionalStatement.cpp
+++ bmiConditionalStatement.cpp
@@ -5,3 +5,6 @@ float calculateBMI(float weight, float height) { // Fungsi untuk menghitung BMI
     return weight / (height * height); // Rumus BMI: berat badan (kg) dibagi dengan tinggi badan (m) kuadrat
 }
 
+string classifyBMI(float bmi) { // Fungsi untuk mengklasifikasikan BMI berdasarkan nilai yang dihitung
+    if (bmi < 18.5) { // Jika BMI kurang dari 18.5, maka status berat badan adalah "Kurang"
+        return "Kurang";
